6b61d362701211025133a73e12c7deb6b347b849
PAVITHRAN <pavithran95palani@gmail.com>
2024-07-24 14:16:26 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+//styles
+import './App.css'
+//contexts
+import { ThemeContext } from './AllContexts';
+//components
+import SideBar from './components/SideBar/SideBar';
+import AppBody from './components/AppBody/AppBody';
+import { updateByLikeDislike } from './functions/functions';
+//assets
+import likeOutlinedIcon from "../src/assets/like-outline-black.svg";
+import dislikeOutlinedIcon from "../src/assets/dislike-outline-black.svg";
+import likeFilledIcon from "../src/assets/like-filled-black.svg";
+import dislikeFilledIcon from "../src/assets/dislike-filled-black.svg";
+
 
 function App() {
+  //states
+  const [theme, setTheme] = useState("light");
+  const [sidebarON, setSideBarON] = useState(false);
+  const [currentChat, setCurrentChat] = useState([]);
+  const [pastConvo, setPastConvo] = useState(false);
+
+  //variables
+  const iconsData = {likeOutlinedIcon, dislikeOutlinedIcon, likeFilledIcon, dislikeFilledIcon};
+  //functions
+  const handleSideBar = () => setSideBarON(!sidebarON);
+  const newChatClick = () => {
+    //save current chat to pastConversations
+    //setCurrentChat to empty []
+    setCurrentChat([]);
+    setPastConvo(false);
+  }
+  const addChatMsg = (userMsg, botReply) => {
+    setCurrentChat([...currentChat, userMsg, botReply]);
+  }
+  const clearCurrentChat = () => setCurrentChat([]);
+  const handlePastConvo = () => setPastConvo(!pastConvo);
+  const likeDislikeReply = (chatCardId, reaction) => setCurrentChat(updateByLikeDislike(chatCardId, reaction, currentChat, iconsData));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <main>
+        <SideBar newChatClick={newChatClick} handleSideBar={handleSideBar} sidebarON={sidebarON} handlePastConvo={handlePastConvo}/>
+        <AppBody likeDislikeReply={likeDislikeReply} pastConvo={pastConvo} clearCurrentChat={clearCurrentChat} addChatMsg={addChatMsg} currentChat={currentChat} handleSideBar={handleSideBar} sidebarON={sidebarON}/>
+      </main>
+    </ThemeContext.Provider>
+    </>
+  )
 }
 
-export default App;
+export default App
